de456cfa5b0d21e0e54f7b9bc79c4bac6433ad68
Emmanuel Agyare <asieduagyare@outlook.com>
2021-01-28 13:14:49 -0700

    Week 4 Assignment Commits

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentListComponent implements OnInit {
 
+  @Output() selectedCDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [
+    new Document('1', 'mannie', 'Make Money', 'https://byui.instructure.com', null),
+    new Document('2', 'stien', 'Make Money', 'https://byui.instructure.com', null),
+    new Document('3', 'CIT', 'Make Money', 'https://byui.instructure.com', null),
+    new Document('4', 'Sam', 'Make Money', 'https://byui.instructure.com', null),
+    new Document('5', 'Jane', 'Make Money', 'https://byui.instructure.com', null),
+    new Document('6', 'Prince', 'Make Money', 'https://byui.instructure.com', null),
+    new Document('7', 'Kyla', 'Make Money', 'https://byui.instructure.com', null)
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelectedDocument(document: Document) {
+    this.selectedCDocumentEvent.emit(document);
+  }
 }
